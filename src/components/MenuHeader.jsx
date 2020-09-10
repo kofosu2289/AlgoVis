d87a30da1716e81022c1c62d382beffd0e24ffad
Kenneth Ofosu <kofosu89@gmail.com>
2020-09-10 18:12:02 -0400

    initial ui

---
+++ src/components/MenuHeader.jsx
@@ -0,0 +1,31 @@
+import React from "react"
+import { CardHeader, Avatar } from "@material-ui/core"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faBriefcase } from "@fortawesome/free-solid-svg-icons"
+import { makeStyles } from "@material-ui/styles"
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    paddingTop: theme.spacing(3),
+    paddingBottom: theme.spacing(3),
+  },
+}))
+
+const MenuHeader = props => {
+  const classes = useStyles()
+
+  return (
+    <CardHeader
+      title="TSP Visualizer"
+      subheader="Visualize various solutions to the trabeling salesperson problem"
+      avatar={
+        <Avatar>
+          <FontAwesomeIcon icon={faBriefcase} />
+        </Avatar>
+      }
+      classes={{ root: classes.root }}
+    />
+  )
+}
+
+export default MenuHeader
